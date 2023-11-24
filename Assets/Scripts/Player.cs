17b87467464ceb8b7df15ffcb5ed7073cd192cb7
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-11-24 15:45:42 +0000

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,21 +15,32 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-
         Debug.Log(isHopping);
 
         if (Input.GetKeyDown(KeyCode.Space) && !isHopping)
         {
             animator.SetTrigger("Hop");
             isHopping = true;
+            float zDifference = 0;
 
-            Debug.Log(transform.position);
-
-            if (transform.position.z % 1 == 0)
+            if (transform.position.z % 1 != 0)
             {
                 Debug.Log("On Grid Space");
-                transform.Translate(new Vector3(1, 0, 0));
+                zDifference = Mathf.Round(transform.position.z) - transform.position.z;
             }
+            transform.position = (transform.position + new Vector3(1, 0, zDifference));
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && !isHopping)
+        {
+            animator.SetTrigger("hop");
+            isHopping = true;
+            transform.position = (transform.position + new Vector3(0, 0, 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
+        {
+            animator.SetTrigger("hop");
+            isHopping = true;
+            transform.position = (transform.position + new Vector3(0, 0, -1));
         }
     }
 
