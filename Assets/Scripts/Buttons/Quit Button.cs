f864202714c34b19f554e5e8707769eee20128ac
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-11-29 13:42:33 +0000

    1

--- Assets/Scripts/Buttons/Quit Button.cs
+++ Assets/Scripts/Buttons/Quit Button.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class QuitButton : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
+    {
+        Quit();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
