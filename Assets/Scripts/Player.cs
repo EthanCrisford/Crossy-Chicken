4b7c371d83b893e659dea8244011decaa779f9ae
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-11-30 12:12:23 +0000

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Animator animator;
-    private bool isHopping;
-
     public GameObject hopButton;
     public GameObject leftButton;
     public GameObject rightButton;
@@ -15,16 +12,13 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-
-        isHopping = false;
 
         hopButton = GameObject.Find("HopButton");
         leftButton = GameObject.Find("LeftHopButton");
         rightButton = GameObject.Find("RightHopButton");
 
-        if( hopButton == null )
+        if (hopButton == null)
         {
             print("didn't find button");
         }
@@ -38,59 +32,20 @@ public class Player : MonoBehaviour
     {
         if (menuButtonScript.hopButtonPressed)
         {
-            rb.velocity = new Vector3(0, 5, 2);
+            rb.velocity = new Vector3(0, 5, 1.5f);
             menuButtonScript.hopButtonPressed = false;
-            print("do hop - player script");
         }
-        
+
         if (menuButtonScript.leftButtonPressed)
         {
             rb.velocity = new Vector3(-2, 0, 0);
             menuButtonScript.leftButtonPressed = false;
-            print("do left - player script");
         }
-        
+
         if (menuButtonScript.rightButtonPressed)
         {
             rb.velocity = new Vector3(2, 0, 0);
             menuButtonScript.rightButtonPressed = false;
-            print("do right - player script");
-        }
-
-        return;
-
-        print("touches=" + Input.touchCount);
-
-        if (Input.touchCount > 0 && !isHopping)
-        {
-            Touch touch = Input.GetTouch(0);
-            animator.SetTrigger("Hop");
-            isHopping = true;
-            float zDifference = 0;
-
-            if (transform.position.z % 1 != 0)
-            {
-                Debug.Log("On Grid Space");
-                zDifference = Mathf.Round(transform.position.z) - transform.position.z;
-            }
-            transform.position = (transform.position + new Vector3(1, 0, zDifference));
-        }
-        else if (Input.GetKeyDown(KeyCode.A) && !isHopping)
-        {
-            animator.SetTrigger("hop");
-            isHopping = true;
-            transform.position = (transform.position + new Vector3(0, 0, 1));
-        }
-        else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
-        {
-            animator.SetTrigger("hop");
-            isHopping = true;
-            transform.position = (transform.position + new Vector3(0, 0, -1));
         }
     }
-
-    public void FinishHop()
-    {
-        isHopping = false;
-    } 
 }
