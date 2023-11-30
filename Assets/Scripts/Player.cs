c6de0b5b19bad0ee650e13acba58e23d9fef536b
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-11-30 13:59:37 +0000

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public bool isGrounded;
     public GameObject hopButton;
     public GameObject leftButton;
     public GameObject rightButton;
     [SerializeField] ButtonScript menuButtonScript;
     Rigidbody rb;
-
+    public Transform rayMarker;
+    public float rayDistance = 0.5f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        isGrounded = true;
         hopButton = GameObject.Find("HopButton");
         leftButton = GameObject.Find("LeftHopButton");
         rightButton = GameObject.Find("RightHopButton");
@@ -21,7 +23,11 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (menuButtonScript.hopButtonPressed)
+        Debug.Log("grounded is" + isGrounded);
+        GroundCheck();
+        Debug.DrawRay(rayMarker.position, -rayMarker.up  * rayDistance);
+
+        if (menuButtonScript.hopButtonPressed && isGrounded)
         {
             rb.velocity = new Vector3(0, 5, 1.5f);
             menuButtonScript.hopButtonPressed = false;
@@ -37,6 +43,22 @@ public class Player : MonoBehaviour
         {
             rb.velocity = new Vector3(2, 0, 0);
             menuButtonScript.rightButtonPressed = false;
+        }
+    }
+
+    void GroundCheck()
+    {
+        Physics.Raycast(rayMarker.position, -Vector3.up, out RaycastHit hitinfo, rayDistance);
+
+        if (hitinfo.collider.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            Debug.Log("Collided with " + hitinfo.collider.name);
+        }
+        else
+        {
+            isGrounded = false;
+            Debug.Log("NOT GROUNDED!");
         }
     }
 }
